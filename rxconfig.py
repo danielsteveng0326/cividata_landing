1e83fe66ca23449059ea6d8d228c2716fe85a290
danielsteveng0326 <danielsteven0326@gmail.com>
2025-06-02 16:24:09 -0500

    config railway deploy

--- rxconfig.py
+++ rxconfig.py
@@ -3,4 +3,8 @@ import reflex as rx
 config = rx.Config(
     app_name="cividata_company_landing_page",
     plugins=[rx.plugins.TailwindV3Plugin()],
+    # Para producción
+    backend_host="0.0.0.0",
+    frontend_port=3000,
+    backend_port=8000,
 )
