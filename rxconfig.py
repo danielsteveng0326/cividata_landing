add6a0eb214a889fb12eb64327f5580f06547497
danielsteveng0326 <danielsteven0326@gmail.com>
2025-06-02 21:15:58 -0500

    Add Dockerfile for Railway deployment

--- rxconfig.py
+++ rxconfig.py
@@ -1,4 +1,5 @@
 import reflex as rx
+import os
 
 config = rx.Config(
     app_name="cividata_company_landing_page",
@@ -6,5 +7,7 @@ config = rx.Config(
     # Para producción
     backend_host="0.0.0.0",
     frontend_port=3000,
-    backend_port=8000,
+    backend_port=int(os.environ.get("PORT", 8000)),
+    # Configuración adicional para Railway
+    api_url=os.environ.get("RAILWAY_PUBLIC_DOMAIN", "localhost:8000"),
 )
