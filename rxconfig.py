3a1d529fefe3c8473ea5284598215ccc5c9b29bd
danielsteveng0326 <danielsteven0326@gmail.com>
2025-06-03 23:30:41 -0500

    fix front cividata ok2

--- rxconfig.py
+++ rxconfig.py
@@ -4,10 +4,6 @@ import os
 config = rx.Config(
     app_name="cividata_company_landing_page",
     plugins=[rx.plugins.TailwindV3Plugin()],
-    # Para producción
     backend_host="0.0.0.0",
-    frontend_port=3000,
-    backend_port=int(os.environ["PORT"]) if "PORT" in os.environ and os.environ["PORT"].isdigit() else 8000,
-    # Configuración adicional para Railway
-    api_url=f"http://0.0.0.0:{os.environ.get('PORT', 8000)}",
+    backend_port=int(os.environ.get("PORT", 3000)),
 )
