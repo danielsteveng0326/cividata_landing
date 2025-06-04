c22710cb20047863a98f0e35e7cec5f80dd92bca
danielsteveng0326 <danielsteven0326@gmail.com>
2025-06-03 22:48:43 -0500

    fix front cividata

--- rxconfig.py
+++ rxconfig.py
@@ -7,7 +7,7 @@ config = rx.Config(
     # Para producción
     backend_host="0.0.0.0",
     frontend_port=3000,
-    backend_port=int(os.environ.get("PORT", 8000)),
+    backend_port=int(os.environ["PORT"]) if "PORT" in os.environ and os.environ["PORT"].isdigit() else 8000,
     # Configuración adicional para Railway
     api_url=f"http://0.0.0.0:{os.environ.get('PORT', 8000)}",
 )
