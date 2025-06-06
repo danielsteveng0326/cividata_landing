644b844863801f25068c00e2b2c3a27ce28b9bc8
danielsteveng0326 <danielsteven0326@gmail.com>
2025-06-06 00:11:07 -0500

    cambio a puerto 3000 rx manual

--- rxconfig.py
+++ rxconfig.py
@@ -1,4 +1,3 @@
-# rxconfig.py
 import reflex as rx
 import os
 
@@ -6,7 +5,6 @@ config = rx.Config(
     app_name="cividata_company_landing_page",
     plugins=[rx.plugins.TailwindV3Plugin()],
     backend_host="0.0.0.0",
-    # Usar el puerto que Railway asigna (3000 en tu caso)
-    backend_port=int(os.environ.get("PORT", 3000)),
-    frontend_port=int(os.environ.get("PORT", 3000)),  # Mismo puerto para ambos
+    backend_port=3000,
+    frontend_port=3000,
 )
